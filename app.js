9caba78503a2a48c9bbc50efc3d3765870fe45b6
nivetha12101997 <nivetha12101997>
2020-10-21 09:16:39 -0700

    add indexof method

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
-//displaying concat()
+/*displaying concat()
 var a=[1,2,3,4,5,6,7];
 var b=[8,9];
 var c=a.concat(b);
-console.log(c);
+console.log(c);*/
+//displaying indexof()
+var  a=[9,7,5,2];
+console.log(a.indexOf(5));
