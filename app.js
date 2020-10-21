f3d766225ad2f056e43b2b772d930f0bcb67559b
nivetha12101997 <nivetha12101997>
2020-10-21 10:08:26 -0700

    add lastindexOf method

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
-/*displaying concat()
-var a=[1,2,3,4,5,6,7];
-var b=[8,9];
+//displaying concat()
+var a=[1,5,5,3,4,5,6,7,5];
+/*var b=[8,9];
 var c=a.concat(b);
 console.log(c);
 //displaying indexof()
 var  a=[9,7,5,2];
-console.log(a.indexOf(5));*/
+console.log(a.indexOf(5));
 //displaying join()
 var a=["social","science","maths","english"];
 var c;
-console.log(c=a.join(" & "));
+console.log(c=a.join(" & "));*/
+//displaying lastindexOf method
+console.log(a.lastIndexOf(5));
