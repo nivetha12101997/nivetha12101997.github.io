cd55beb16998a24444644047ed6437170c4635d1
nivetha12101997 <nivetha12101997>
2020-10-21 11:45:10 -0700

    add splicing method

--- app.js
+++ app.js
@@ -22,12 +22,16 @@ console.log(colors.sort());
 var days=["mon","tues","wed"];
 //console.log(days.toString());
 //displaying valueOf method
-console.log(days.valueOf());*/
+console.log(days.valueOf());
 //displaying slice method
 var student=["mano","kavi","savitha","mary","tina","emy","query"];
 console.log(student.slice(0,5));
 console.log(student.slice(-5,-3));
 console.log(student.slice(-5,3));//if end value does not cross the start value,slicing is not possible
-console.log(student.slice(2,-2));
+console.log(student.slice(2,-2));*/
+
+//display splicing method
+var num=[10,20,60,80,90];
+console.log(num.splice(0,3,"50,70"));
 
 
